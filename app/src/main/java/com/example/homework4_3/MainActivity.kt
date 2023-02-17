8e6e882b069b7fa56f902a6eff248b0205c30966
koster06 <46241269+koster06@users.noreply.github.com>
2023-02-17 14:19:49 +0300

    add intent & radioButtons & email field

--- app/src/main/java/com/example/homework4_3/MainActivity.kt
+++ app/src/main/java/com/example/homework4_3/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.homework4_3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+const val NAME = "name"
+const val PHONE = "phone"
+const val EMAIL = "email"
 
 class MainActivity : AppCompatActivity() {
+
+    var etName: EditText? = null
+    var etPhone: EditText? = null
+    var etEmail: EditText? = null
+    var btnR1: Button? = null
+    var btnR2: Button? = null
+    var btn: Button? = null
+
+    private val textWatcher: TextWatcher = object: TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            val nameFilled = etName?.text.toString()
+            val phoneFilled = etPhone?.text.toString()
+            val emailFilled = etEmail?.text.toString()
+
+            btn?.setEnabled(!nameFilled.isEmpty() && !phoneFilled.isEmpty() && !emailFilled.isEmpty())
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intentToAnotherScreen = Intent(applicationContext, MainActivity2::class.java)
+
+        etName = findViewById(R.id.editTextTextPersonName)
+        etPhone = findViewById(R.id.editTextPhone)
+        btn = findViewById(R.id.button)
+        btnR1 = findViewById(R.id.radioButton)
+        btnR2 = findViewById(R.id.radioButton2)
+
+        this.etName?.addTextChangedListener(textWatcher)
+        this.etPhone?.addTextChangedListener(textWatcher)
+
+        btn?.setOnClickListener {
+            intentToAnotherScreen.putExtra(NAME,etName?.text.toString())
+            intentToAnotherScreen.putExtra(PHONE, etPhone?.text.toString())
+            intentToAnotherScreen.putExtra(EMAIL, etEmail?.text.toString())
+            startActivity(intentToAnotherScreen)
+            Toast.makeText(this@MainActivity, "You are going to the second screen", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
+
+/*
+Взять активити из ДЗ №3
+Введенные данные пользователя с помощью intent передать во вторую активи
+Используйте приведение типов (возраст и номер телефона передайте как int)
+Добавить radiobutton для выбора пола
+Добавить поле добавить поле для ввода email (необходима валидация email)
+Добавить поле выбора даты рождения. При нажатии на поле открывается календарь
+Добавить кнопку для перехода на 3-юю активити
+При нажатии на кнопку должна открываться 3-яя активити
+С помощью intent передать в нее данные из первой и второй активити.
+Пол передать как параметр boolean означающий выбран женский пол или мужской
+(например isMen или isFemail)
+В 3-ей активити отобразить переданные данные в таблице (как было в дз №3)
+В 3-ей активити в зависимости от выбранного пола применить стиль.
+ */
---
+++ app/src/main/java/com/example/homework4_3/MainActivity2.kt
@@ -0,0 +1,35 @@
+package com.example.homework4_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main2)
+
+            val name = intent?.getStringExtra(NAME)
+            val phone = intent?.getStringExtra(PHONE)?.toInt()
+            val email = intent?.getStringExtra(EMAIL)
+
+            val textView: TextView = findViewById(R.id.textView)
+            textView.text = "  Your name: $name  \n  Your phone: $phone \n  Your email: $email"
+        }
+}
+/*
+Взять активити из ДЗ №3 +
+Введенные данные пользователя с помощью intent передать во вторую активи +
+Используйте приведение типов (возраст и номер телефона передайте как int)
+Добавить radiobutton для выбора пола
+Добавить поле для ввода email (необходима валидация email)
+Добавить поле выбора даты рождения. При нажатии на поле открывается календарь
+Добавить кнопку для перехода на 3-юю активити
+При нажатии на кнопку должна открываться 3-яя активити
+С помощью intent передать в нее данные из первой и второй активити.
+Пол передать как параметр boolean означающий выбран женский пол или мужской
+(например isMen или isFemail)
+В 3-ей активити отобразить переданные данные в таблице (как было в дз №3)
+В 3-ей активити в зависимости от выбранного пола применить стиль.
+ */
